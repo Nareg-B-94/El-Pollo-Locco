60511d1b85683e55d3ce20e87aed75358f094873
Nareg Berberian <naregb94@gmail.com>
2022-07-08 12:42:25 +0200

    first commit v3

---
+++ js/game.js
@@ -0,0 +1,11 @@
+// let canvas;
+// let ctx;
+// let character = new Image();
+
+// function init() {
+//     canvas = document.getElementById('canvas');
+//     ctx = canvas.getContext('2d');
+//     // character.src = '../';
+
+//     ctx.drawImage(character, 20, 20, 50, 150);
+// }
