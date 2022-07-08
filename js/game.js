ccfc0140bcddeaab4d37740103aaadb6fb818640
Nareg Berberian <naregb94@gmail.com>
2022-07-08 13:07:48 +0200

    started with Models

--- js/game.js
+++ js/game.js
@@ -1,11 +1,12 @@
-// let canvas;
-// let ctx;
-// let character = new Image();
+let canvas;
+let ctx;
+let character = new movableObject();
 
-// function init() {
-//     canvas = document.getElementById('canvas');
-//     ctx = canvas.getContext('2d');
-//     // character.src = '../';
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
-//     ctx.drawImage(character, 20, 20, 50, 150);
-// }
+    console.log('my character is', character)
+
+
+}
---
+++ models/movableObject.class.js
@@ -0,0 +1,9 @@
+class movableObject {
+    x = 120;
+    y = 400;
+    img;
+
+    moveRight() {
+console.log('moving right')
+    };
+}
